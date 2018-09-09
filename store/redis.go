993d6f3331e09ccbbd3cbf8454cc3762b6611057
Jacob Hackett <jacobrhackett@gmail.com>
2018-09-09 10:56:44 -0400

    dont export redis client struct

--- store/redis.go
+++ store/redis.go
@@ -1,19 +1,16 @@
 package store
 
-import (
-	"github.com/go-redis/redis"
-)
+import "github.com/go-redis/redis"
 
 type (
-	// Redis is the wrapper around the redis client
-	Redis struct {
+	redisClient struct {
 		Client *redis.Client
 	}
 )
 
 // New returns a new Redis implementation
 func New() Store {
-	return &Redis{
+	return &redisClient{
 		Client: redis.NewClient(&redis.Options{
 			Addr:     "redis:6379",
 			Password: "", // no password set
@@ -23,26 +20,26 @@ func New() Store {
 }
 
 // Ping pings the redis instance
-func (r Redis) Ping() (string, error) {
+func (r redisClient) Ping() (string, error) {
 	return r.Client.Ping().Result()
 }
 
 // Get returns the value for the key and field
-func (r Redis) Get(key, field string) (string, error) {
+func (r redisClient) Get(key, field string) (string, error) {
 	return r.Client.HGet(key, field).Result()
 }
 
 // GetAll returns all values for the given key
-func (r Redis) GetAll(key string) (map[string]string, error) {
+func (r redisClient) GetAll(key string) (map[string]string, error) {
 	return r.Client.HGetAll(key).Result()
 }
 
 // Set is used to set a value for a specific field under a key
-func (r Redis) Set(key, field string, value interface{}) error {
+func (r redisClient) Set(key, field string, value interface{}) error {
 	return r.Client.HSet(key, field, value).Err()
 }
 
 // Delete deletes a field within a key
-func (r Redis) Delete(key, field string) error {
+func (r redisClient) Delete(key, field string) error {
 	return r.Client.HDel(key, field).Err()
 }
